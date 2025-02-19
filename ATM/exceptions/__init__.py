6cde2610972e12891251c640b899e8235d0a268c
Pranaov S <148376495+pranaovs@users.noreply.github.com>
2025-02-19 09:29:03 +0530

    move exceptions to init

---
+++ ATM/exceptions/__init__.py
@@ -0,0 +1,18 @@
+class AccountNotFound(Exception):
+    pass
+
+
+class MultipleAccounts(Exception):
+    pass
+
+
+class BalanceInsufficient(Exception):
+    pass
+
+
+class InvalidAmount(Exception):
+    pass
+
+
+class InvalidPin(Exception):
+    pass
