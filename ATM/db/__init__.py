3ebc8c1871a68b450886ec836e01dc5fc89b949c
Pranaov S <148376495+pranaovs@users.noreply.github.com>
2025-02-19 09:28:55 +0530

    add check pin function in db

--- ATM/db/__init__.py
+++ ATM/db/__init__.py
@@ -1,24 +1,25 @@
-from tinydb import TinyDB, Query
-from tinydb.table import Document
+from tinydb import Query, TinyDB
 from tinydb.operations import add, subtract
-from ..exceptions.exceptions import (
+from tinydb.table import Document
+
+from ..exceptions import (
     AccountNotFound,
-    MultipleAccounts,
     BalanceInsufficient,
     InvalidAmount,
+    MultipleAccounts,
 )
 
 
-class database:
+class Database:
     def __init__(self) -> None:
-        self.db = TinyDB("atm_db.json")
+        self._db = TinyDB("atm_db.json")
         query = Query()
 
     def query(self, accid: int) -> dict[str, int | str]:
         accid = int(accid)
 
-        if result := self.db.get(doc_id=accid):
-            return dict(result)
+        if result := self._db.get(doc_id=accid):
+            return dict(result)  # type: ignore
 
         else:
             raise AccountNotFound(f"Account id not found: {accid}")
@@ -30,21 +31,22 @@ class database:
         balance = int(details["balance"])
         return int(balance)  # type: ignore
 
-    def new(self, name: str, phone: int) -> int:
+    def new(self, name: str, phone: int, pin: str) -> int:
 
         # TODO: add date to transactions list
         data = {
             "name": str(name),
             "phone": int(phone),
             "balance": 0,
+            "pin": str(pin),
             "transactions": [],
         }
 
-        return int(self.db.insert(data))
+        return int(self._db.insert(data))
 
     def update(self, accid, key, value):
         accid = int(accid)
-        self.db.update(Document({key: value}, doc_id=accid))
+        self._db.update(Document({key: value}, doc_id=accid))
 
     def withdraw(self, accid: int, amount: int) -> int:
         accid = int(accid)
@@ -58,7 +60,7 @@ class database:
         if balance < 0:
             raise BalanceInsufficient(balance)
 
-        self.db.update(subtract(accid, amount))
+        self._db.update(subtract(accid, amount))
         return balance
 
     def deposit(self, accid: int, amount: int) -> int:
@@ -70,7 +72,7 @@ class database:
 
         balance = int(details.get("Balance")) - int(amount)  # type: ignore
 
-        self.db.update(add(accid, amount))
+        self._db.update(add(accid, amount))
 
         return balance
 
@@ -84,4 +86,21 @@ class database:
             )
 
         else:
-            self.db.remove(doc_ids=[accid])
+            self._db.remove(doc_ids=[accid])
+
+    def check_pin(self, accid: int, pin: str) -> bool:
+        accid = int(accid)
+        details = self.query(accid=accid)
+        if str(pin) == details.get("pin"):
+            return True
+        else:
+            return False
+
+    def set_pin(self, accid: int, pin: str) -> None:
+        accid = int(accid)
+        self.update(accid, "pin", str(pin))
+
+
+@dataclass
+class Transaction:
+    pass
